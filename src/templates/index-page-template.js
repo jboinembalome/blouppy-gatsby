009158fdd5e17d3c83f85035ce6fbc1829122bb5
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-03-13 17:51:46 +0100

    Update container in landing page

--- src/templates/index-page-template.js
+++ src/templates/index-page-template.js
@@ -12,34 +12,32 @@ export const IndexPageTemplate = ({
   mainpitch,
 }) => (
   <div>
-    <div className="relative overflow-hidden">
-      <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-            <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">{title}</span>
-              </h1>
-              <h3 className="mt-3 text-base text-violet-600 dark:text-violet-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                {subheading}
-              </h3>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+    <section className="flex justify-center pt-10 pb-20 px-4 sm:px-6 lg:pt-14 lg:pb-28 lg:px-8">
+      <div className="container">
+        <div className="max-w-7xl mx-auto mb-20 lg:grid lg:grid-cols-12 lg:gap-x-8">
+          <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:place-self-center">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-5xl md:text-6xl">
+              <span className="block xl:inline">{title}</span>
+            </h1>
+            <h3 className="mt-3 text-base text-violet-600 dark:text-violet-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              {subheading}
+            </h3>
+          </div>
+          <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+            <div className="relative mx-auto w-full lg:max-w-md">
+              <div
+                className="relative block w-full rounded-lg overflow-hidden"
+              >
+                <PreviewCompatibleImage className="w-full"
+                  imageInfo={{
+                    image: !!image.childImageSharp ? getSrc(image.childImageSharp.gatsbyImageData) : image,
+                    alt: "",
+                  }}
+                />
               </div>
             </div>
-          </main>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center mt-2 lg:absolute lg:inset-y-0 lg:right-0 lg:mr-6">
-        <PreviewCompatibleImage className="h-full w-2/3 object-cover lg:w-full lg:h-full"
-          imageInfo={{
-            image: !!image.childImageSharp ? getSrc(image.childImageSharp.gatsbyImageData) : image,
-            alt: "",
-          }}
-        />
-      </div>
-    </div>
-    <section className="flex justify-center pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-      <div className="container">
         <div className="flex-row">
           <div className="flex-col space-y-8">
             <div className="text-lg mx-auto">
