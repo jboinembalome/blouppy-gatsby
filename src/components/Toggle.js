3ad96f142e7c1cc33def0341a787ba60545e173f
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-03-20 09:40:20 +0100

    Update color

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -11,7 +11,7 @@ const Toggle = (props) => {
         onChange={event => props.onChange(event)}
         className={classNames(
             props.enabled ? 'bg-violet-600' : 'bg-amber-400',
-            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500'
         )}
     >
         <span className="sr-only">Use setting</span>
