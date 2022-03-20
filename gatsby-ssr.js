948b94a061bd0158c9f3fe9bb77369ace4ddb3ba
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-03-20 20:23:35 +0100

    Add buymeacoffee link

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,10 @@
+const React = require("react");
+
 exports.replaceRenderer = ({ replaceBodyHTMLString }) => {
-    replaceBodyHTMLString('<div id="___gatsby"></div>');
-  };
+  replaceBodyHTMLString('<div id="___gatsby"></div>');
+};
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="blouppy" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>]);
+};
