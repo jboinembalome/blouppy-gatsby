8c65dfc49b0302974205d2beb50ad16b4c4c4623
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-04-03 12:01:44 +0200

    Load theme before buymeacoffee

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,10 +6,9 @@ exports.replaceRenderer = ({ replaceBodyHTMLString }) => {
 
 exports.onRenderBody = ({ setPostBodyComponents }) => {
   setPostBodyComponents([
-    <script key="bmc-widget-script" data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="blouppy" data-description="Support me on Buy me a coffee!" data-message="" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>,
     <script key="theme-initialize"
-    dangerouslySetInnerHTML={{
-      __html: `
+      dangerouslySetInnerHTML={{
+        __html: `
         try {
           if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark')
@@ -18,6 +17,18 @@ exports.onRenderBody = ({ setPostBodyComponents }) => {
           }
         } catch (_) {}
       `,
-    }}
-  />]);
+      }}
+    />,
+    <script key="bmc-widget-script"
+      data-name="BMC-Widget"
+      data-cfasync="false"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+      data-id="blouppy"
+      data-description="Support me on Buy me a coffee!"
+      data-message=""
+      data-color="#BD5FFF"
+      data-position="Right"
+      data-x_margin="18"
+      data-y_margin="18"></script>,
+  ]);
 };
