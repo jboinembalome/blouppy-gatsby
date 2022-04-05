c6f7f7aabe20b3a11099255eed047549c0b5b875
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-04-05 19:07:55 +0200

    Remove cloudbinary and uploadcare

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,15 +1,10 @@
 import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import ResumePagePreview from './preview-templates/ResumePagePreview'
 import PortfolioPostPreview from './preview-templates/PortfolioPostPreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('resume', ResumePagePreview)
