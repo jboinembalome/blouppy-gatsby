3f4d5b8dc66c7f458c7c542f309b1abcb311f2b0
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-29 00:23:24 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,15 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-RDNQF6X1VT", // Google Analytics / GA
+        ],
+      },
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
