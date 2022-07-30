35ba758c3c274c13ae62f2567ff650d47f95e73f
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-30 18:33:31 +0200

    Fix skills styling and add not-unreset

--- src/templates/resume-template.js
+++ src/templates/resume-template.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Content from '../components/Content'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
-import EnglishResumeJB from '../../static/downloads/CV_EN_Boinembalome_Jimmy.pdf' 
-import FrenchResumeJB from '../../static/downloads/CV_FR_Boinembalome_Jimmy.pdf' 
+import EnglishResumeJB from '../../static/downloads/CV_EN_Boinembalome_Jimmy.pdf'
+import FrenchResumeJB from '../../static/downloads/CV_FR_Boinembalome_Jimmy.pdf'
 
 export const ResumePageTemplate = ({ title, subtitle, resumeimage, content, contentComponent, helmet }) => {
   const PageContent = contentComponent || Content
@@ -153,38 +153,39 @@ export const ResumePageTemplate = ({ title, subtitle, resumeimage, content, cont
 
             </div>
             <PageContent className="unreset prose lg:prose-lg dark:prose-invert prose-indigo" content={content} />
-          </div>
-          <div className="unreset prose lg:prose-lg dark:prose-invert prose-indigo text-gray-500 dark:text-gray-400 mx-auto">
-            <h1>Skills</h1>
-            <h2>Technical Skills</h2>
-            <ul className=" mt-2 pl-0 leading-8">
-              {skills.specifics.map((skill) => (
-                <li key={skill + `skill`} className="no-underline inline-flex items-center mr-2 px-3 py-0.5 rounded-full text-base font-medium bg-violet-100 text-violet-800">
-                  <span>{skill}</span>
-                </li>
-              ))}
-            </ul>
-            <h2>Soft Skills</h2>
-            <ul className=" mt-2 pl-0 leading-8">
-              {skills.softs.map((soft) => (
-                <li key={soft + `soft`} className="no-underline inline-flex items-center mr-2 px-3 py-0.5 rounded-md text-base font-medium bg-amber-100 text-amber-800">
-                  <span>{soft}</span>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-12 unreset prose lg:prose-lg dark:prose-invert prose-indigo">
-              <h1>Download</h1>
-              <p>If you wish, you can download my resume: </p>
-              <div className="mt-6 space-y-4 flex flex-col justify-between sm:space-y-0 sm:flex-row">
-                <a href={EnglishResumeJB} download className="no-underline inline-flex items-center justify-center mr-2 px-4 py-2 text-base font-medium rounded-md shadow-sm text-white dark:text-gray-900 bg-violet-600 hover:bg-violet-700 dark:bg-violet-400 hover:dark:bg-violet-500">
-                  Download In English
-                </a>
-                <a href={FrenchResumeJB} download className="no-underline inline-flex items-center justify-center mr-2 px-4 py-2 text-base font-medium rounded-md shadow-sm text-white dark:text-gray-900 bg-violet-600 hover:bg-violet-700 dark:bg-violet-400 hover:dark:bg-violet-500">
-                  Download In French
-                </a>
+            <div className="unreset prose lg:prose-lg dark:prose-invert prose-indigo">
+              <h1>Skills</h1>
+              <h2>Technical Skills</h2>
+              <ul className="mt-2 pl-0 leading-8">
+                {skills.specifics.map((skill) => (
+                  <li key={skill + `skill`} className="not-prose no-underline inline-flex items-center mr-2 px-3 py-0.5 rounded-full text-base font-medium bg-violet-100 text-violet-800">
+                    <span>{skill}</span>
+                  </li>
+                ))}
+              </ul>
+              <h2>Soft Skills</h2>
+              <ul className="mt-2 pl-0 leading-8">
+                {skills.softs.map((soft) => (
+                  <li key={soft + `soft`} className="not-prose no-underline inline-flex items-center mr-2 px-3 py-0.5 rounded-md text-base font-medium bg-amber-100 text-amber-800">
+                    <span>{soft}</span>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-12 unreset prose lg:prose-lg dark:prose-invert prose-indigo">
+                <h1>Download</h1>
+                <p>If you wish, you can download my resume: </p>
+                <div className="mt-6 space-y-4 flex flex-col justify-between sm:space-y-0 sm:flex-row">
+                  <a href={EnglishResumeJB} download className="not-unreset no-underline inline-flex items-center justify-center mr-2 px-4 py-2 text-base font-medium rounded-md shadow-sm text-white dark:text-gray-900 bg-violet-600 hover:bg-violet-700 dark:bg-violet-400 hover:dark:bg-violet-500">
+                    Download In English
+                  </a>
+                  <a href={FrenchResumeJB} download className="not-unreset no-underline inline-flex items-center justify-center mr-2 px-4 py-2 text-base font-medium rounded-md shadow-sm text-white dark:text-gray-900 bg-violet-600 hover:bg-violet-700 dark:bg-violet-400 hover:dark:bg-violet-500">
+                    Download In French
+                  </a>
+                </div>
               </div>
-            </div> 
+            </div>
           </div>
+
         </div>
       </div>
     </div>
