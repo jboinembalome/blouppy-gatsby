46a43fcd30715221b40909d8932183839d3c92bc
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-07-30 15:53:14 +0200

    Update tailwind config according to new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,8 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  experimental: {},
-  future: {},
   darkMode: 'class',
-  important: true,
   content: ["./src/**/*.{js,jsx,md}"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.slate,
-      teal: colors.teal,
-      green: colors.green,
-      amber: colors.amber,
-      orange: colors.orange,
-      violet: colors.violet,
-      purple: colors.purple,
-      pink: colors.pink,
-      rose: colors.rose,
-      indigo: colors.indigo,
-      red: colors.red,
-      yellow: colors.yellow,
-      blue: colors.blue
-    }
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
