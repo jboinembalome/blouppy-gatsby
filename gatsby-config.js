a3d21c3c479a73c27a4c61da961b6de4c2c24fd9
Jimmy Boinembalome <jboinembalome@gmail.com>
2022-12-25 21:22:28 +0100

    Override style for gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,7 @@ module.exports = {
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
               maxWidth: 2048,
+              backgroundColor: 'transparent'
             },
           },
           {
