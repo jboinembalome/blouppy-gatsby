d5b304929d5996a81abb3a09b38ddb7d81c30131
Jimmy Boinembalome <jboinembalome@gmail.com>
2023-04-22 18:42:46 +0200

    Update Card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -7,10 +7,8 @@ export interface CardProps {
 
 export const Card = ({ children, className }: CardProps) => {
     return (
-        <>
-            <div className={className}>
-                {children}
-            </div>
-        </>    
+        <div className={className}>
+            {children}
+        </div>
     );
 };
