9da716bd9af8a948a37dac6e49e8a0feb14be208
Jimmy Boinembalome <jboinembalome@gmail.com>
2023-05-19 14:59:54 +0200

    Add third blur circle

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -44,6 +44,8 @@ export const Layout = ({ children }: LayoutProps) => {
       <Navbar />
       <div className="fixed left-1/2 top-1/2 h-96 w-96 -translate-x-64 -translate-y-64 scale-125 rounded-full bg-primary-500/30 blur-[100px]"></div>
       <div className="fixed left-1/2 top-1/2 h-96 w-96 scale-125 rounded-full bg-primary-200/20 blur-[100px]"></div>
+      <div className="fixed left-1/2 top-1/2 h-96 w-96 -translate-x-96 scale-125 rounded-full bg-primary-200/20 blur-[100px]"></div>
+
       <div className="relative max-w-7xl mx-auto p-8">{children}</div>
       <Footer />
     </div>
