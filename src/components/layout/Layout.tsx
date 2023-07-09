584fb3be106f5a1a5c6dda6809c0e651f0f27a3e
Jimmy Boinembalome <jboinembalome@gmail.com>
2023-07-09 10:43:18 +0200

    Add min-h-screen in Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -41,10 +41,12 @@ export const Layout = ({ children }: LayoutProps) => {
         />
         <body />
       </Helmet>
-      <Navbar />
-      <div className="fixed left-1/2 top-1/2 h-96 w-96 -translate-x-64 -translate-y-64 scale-125 rounded-full bg-indigo-800/30 blur-[150px] dark:blur-[100px]"></div>
-      <div className="relative max-w-7xl mx-auto p-8 main">{children}</div>
-      <Footer />
+      <div className="min-h-screen">
+        <Navbar />
+        <div className="fixed left-1/2 top-1/2 h-96 w-96 -translate-x-64 -translate-y-64 scale-125 rounded-full bg-indigo-800/30 blur-[150px] dark:blur-[100px]"></div>
+        <div className="relative max-w-7xl mx-auto p-8 main">{children}</div>
+        <Footer />
+      </div>
     </div>
   );
 };
